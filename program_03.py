369157748f9e7b2cd9f0e12239f472abfc2f0e77
MagicalMaker <108678994+MagicalMaker@users.noreply.github.com>
2023-07-12 08:44:44 +0530

    Add files via upload

---
+++ program_03.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+if i == 23:
+    break;
+
+
+# In[4]:
+
+
+if i = 78:
+    break
+    
+
+
+# In[5]:
+
+
+if i = 78:
+    continue
+    
+
+
+# In[7]:
+
+
+def greet():
+    print("good mroni")
+    print("priyanshi")
+    print("oooooooooooo")
+greet()
+
+
+# In[9]:
+
+
+def priyanshi():
+    print("good")
+    print("morning")
+    print("goluuu")
+priyanshi()
+
+
+# In[15]:
+
+
+class employee:
+    def __init__(self,gname,gsalary):
+        self.name = gname
+        self.salary= gsalary
+pri= employee ("pri",140)
+print(pri.name)
+print(pri.salary)
+
+
+# In[22]:
+
+
+def sum(a,b):
+    a=1
+    b=2
+    sum=a+b
+    return sum
+
+
+# In[23]:
+
+
+def sum(a,b):
+    c=a+b
+    return c
+d=(1,2)
+print(d)
+
+
+# In[25]:
+
+
+def sum (a,b):
+    a=1
+    b=2
+    c=a+b
+    print(c)
+    
+
+
+# In[26]:
+
+
+class pri:
+    def __init__ (gname,gsalary):
+        self.name=gname
+        self.salary=gsalary
+pri = pri("priyanshi",12)
+print(pri.name)
+print(pri.salary)    
+
+
+# In[33]:
+
+
+class employee:
+    def __init__(gname,gage):
+        self.name=gname
+        self.age=gage
+        
+    pri = employee("priya",12)
+print(pri.name)
+print(pri.age)
+
+
+# In[35]:
+
+
+class employee :
+    def __init__(self,gname,gsalary):
+        self.name=gname
+        self.salary=gsalary
+pri=employee("pri",22)
+print(pri.name)
+print(pri.salary)
+
+
+# In[36]:
+
+
+class diya:
+    def __init__(self,gname,gsalary):
+        self.name=gname
+        self.salary=gsalary
+pri = diya("priya",22)
+print(pri.name)
+print(pri.salary)
+
+
+# In[37]:
+
+
+a = int(input("enter a sequence"))
+b=a[::-1]
+if a==b:
+    print("palindrome")
+else:
+    print("no palindrome")
+
+
+# In[38]:
+
+
+a = int(input("enter a sequence"))
+b=a[::-1]
+if a==b:
+    print("palindrome")
+else:
+    print("no palindrome")
+
+
+# In[39]:
+
+
+a = int(input("enter a sequence"))
+b=a[::-1]
+if a==b:
+    print("palindrome")
+else:
+    print("no palindrome")
+    
+
+
+# In[ ]:
+
+
+
+
