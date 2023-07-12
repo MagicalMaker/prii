61b58b0dcfaa6ab28273f44bbadbc24cfce24585
MagicalMaker <108678994+MagicalMaker@users.noreply.github.com>
2023-07-12 09:50:25 +0530

    Add files via upload

---
+++ program_03 (1).py
@@ -0,0 +1,409 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+if i == 23:
+    break;
+
+
+# In[4]:
+
+
+if i = 78:
+    break
+    
+
+
+# In[5]:
+
+
+if i = 78:
+    continue
+    
+
+
+# In[7]:
+
+
+def greet():
+    print("good mroni")
+    print("priyanshi")
+    print("oooooooooooo")
+greet()
+
+
+# In[9]:
+
+
+def priyanshi():
+    print("good")
+    print("morning")
+    print("goluuu")
+priyanshi()
+
+
+# In[15]:
+
+
+class employee:
+    def __init__(self,gname,gsalary):
+        self.name = gname
+        self.salary= gsalary
+pri= employee ("pri",140)
+print(pri.name)
+print(pri.salary)
+
+
+# In[22]:
+
+
+def sum(a,b):
+    a=1
+    b=2
+    sum=a+b
+    return sum
+
+
+# In[23]:
+
+
+def sum(a,b):
+    c=a+b
+    return c
+d=(1,2)
+print(d)
+
+
+# In[25]:
+
+
+def sum (a,b):
+    a=1
+    b=2
+    c=a+b
+    print(c)
+    
+
+
+# In[26]:
+
+
+class pri:
+    def __init__ (gname,gsalary):
+        self.name=gname
+        self.salary=gsalary
+pri = pri("priyanshi",12)
+print(pri.name)
+print(pri.salary)    
+
+
+# In[33]:
+
+
+class employee:
+    def __init__(gname,gage):
+        self.name=gname
+        self.age=gage
+        
+    pri = employee("priya",12)
+print(pri.name)
+print(pri.age)
+
+
+# In[35]:
+
+
+class employee :
+    def __init__(self,gname,gsalary):
+        self.name=gname
+        self.salary=gsalary
+pri=employee("pri",22)
+print(pri.name)
+print(pri.salary)
+
+
+# In[36]:
+
+
+class diya:
+    def __init__(self,gname,gsalary):
+        self.name=gname
+        self.salary=gsalary
+pri = diya("priya",22)
+print(pri.name)
+print(pri.salary)
+
+
+# In[37]:
+
+
+a = int(input("enter a sequence"))
+b=a[::-1]
+if a==b:
+    print("palindrome")
+else:
+    print("no palindrome")
+
+
+# In[38]:
+
+
+a = int(input("enter a sequence"))
+b=a[::-1]
+if a==b:
+    print("palindrome")
+else:
+    print("no palindrome")
+
+
+# In[39]:
+
+
+a = int(input("enter a sequence"))
+b=a[::-1]
+if a==b:
+    print("palindrome")
+else:
+    print("no palindrome")
+    
+
+
+# In[40]:
+
+
+a = input("enter a sequence")
+b=a[::-1]
+if a==b:
+    print("palindrome")
+else:
+    print("no palindrome")
+
+
+# In[41]:
+
+
+'''this is us'''
+
+
+# In[42]:
+
+
+print(123)
+
+
+# In[43]:
+
+
+print("123")
+
+
+# In[44]:
+
+
+print('123')
+
+
+# In[ ]:
+
+
+#data types and variables 
+#int ---> numbers (0-9)
+#float ---> 
+#character 
+#string 
+#complex
+
+
+# In[45]:
+
+
+a=19
+print(a)
+print('a')
+
+
+# In[46]:
+
+
+print(type)
+
+
+# In[47]:
+
+
+type(a)
+
+
+# In[48]:
+
+
+type(u)
+
+
+# In[ ]:
+
+
+#float
+
+
+# In[49]:
+
+
+a=54.4
+print(a)
+
+
+# In[50]:
+
+
+type(a)
+
+
+# In[ ]:
+
+
+#string
+
+
+# In[52]:
+
+
+a="yyyyyyyyyy"
+print(a)
+
+
+# In[53]:
+
+
+type(a)
+
+
+# In[ ]:
+
+
+#boolean ---> TRUE & FALSE
+
+
+# In[57]:
+
+
+a='True'
+print(a)
+type(a)
+
+
+# In[56]:
+
+
+a=True
+print(a)
+type(a)
+
+
+# In[61]:
+
+
+a=False
+print(a)
+type(a)
+
+
+# In[64]:
+
+
+a='false'
+print(a)
+type(a)
+
+
+# In[63]:
+
+
+a='false'
+print(a)
+type(a)
+
+
+# In[ ]:
+
+
+#complex
+
+
+# In[69]:
+
+
+x= 7+2j
+print(x)
+type(x)
+
+
+# In[70]:
+
+
+#wap to add two numbers
+a=2
+b=3
+c=a+b
+print(c)
+
+
+# In[72]:
+
+
+#wap to swap two numbers
+#1.with the help of third variable 
+#2. without using third varaible
+a=2
+b=1
+a,b=b,a
+print(a,b)
+print(b,a)
+
+
+# In[76]:
+
+
+#user input 
+#user input --->
+x=input("enter something")
+print(x)
+
+
+
+
+# In[80]:
+
+
+#wap to add two number with help user input 
+a=int(input("enter a number 1"))
+b=int(input("enter a number 2"))
+c=int(a+b)
+print(c)
+
+
+# In[78]:
+
+
+a='3'
+b='1'
+c=a+b
+print(c) #concatenation
+
+
+# In[83]:
+
+
+a=int(input("enter a number"))
+b=str(input("enter a  sec number")) 
+c=a+b
+print(c)
+
+
+# In[ ]:
+
+
+
+
