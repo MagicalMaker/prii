989c5613e55ead7c62708e972e620385536374e3
MagicalMaker <108678994+MagicalMaker@users.noreply.github.com>
2023-07-13 13:48:39 +0530

    Add files via upload

---
+++ basic_04.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello")
+
+
+# In[2]:
+
+
+hello world
+
+
+# In[3]:
+
+
+'''p'''
+
+
+# In[4]:
+
+
+'''h
+e
+l
+l
+o'''
+
+
+# In[5]:
+
+
+print("hello world")
+
+
+# In[6]:
+
+
+print('hello world')
+
+
+# In[7]:
+
+
+print('''hello''')
+
+
+# In[8]:
+
+
+print(90)
+
+
+# In[10]:
+
+
+x=12
+print(x)
+
+
+# In[11]:
+
+
+type(x)
+
+
+# In[12]:
+
+
+x=-12
+print(x)
+
+
+# In[13]:
+
+
+type(x)
+
+
+# In[14]:
+
+
+12
+
+
+# In[15]:
+
+
+123
+
+
+# In[16]:
+
+
+x=23.4
+print(x)
+
+
+# In[17]:
+
+
+type(x)
+
+
+# In[18]:
+
+
+x="hello"
+print(x)
+
+
+# In[19]:
+
+
+x='hello'
+print(x)
+
+
+# In[20]:
+
+
+type(x)
+
+
+# In[21]:
+
+
+a=123
+print(a)
+
+
+# In[22]:
+
+
+type(a)
+
+
+# In[23]:
+
+
+a=2
+b=3
+c=a+b
+print(c)
+
+
+# In[24]:
+
+
+a1=2
+a2=3
+c=a1+a2
+print(c)
+
+
+# In[25]:
+
+
+A=2
+B=3
+C=A+B
+PRINT(C)
+
+
+# In[28]:
+
+
+a=9
+b=5
+a,b=b,a
+print(a,b)
+print(b,a)
+
+
+# In[ ]:
+
+
+
+
